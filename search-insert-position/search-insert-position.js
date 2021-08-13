13c987ebcf5818f699841645e40912389dc6088d
ctan1695 <71043594+ctan1695@users.noreply.github.com>
2021-08-13 16:39:58 -0500

    Time: 92 ms (7.47%), Space: 39.4 MB (92.62%) - LeetHub

---
+++ search-insert-position/search-insert-position.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let minIndex = 0;
+    let maxIndex = nums.length - 1;
+    let guessIndex;
+    
+    while (true) {
+        guessIndex = Math.floor((minIndex + maxIndex)/2);
+        
+        if (nums[guessIndex] === target) {
+            return guessIndex;
+        }
+
+        if (minIndex >= maxIndex && nums[guessIndex] !== target) {
+            if (target > nums[minIndex]) {
+            return minIndex + 1;
+            } else {
+                return minIndex;
+            }
+        }        
+        
+        if (nums[guessIndex] < target) {
+            minIndex = guessIndex + 1;
+        } else {
+            maxIndex = guessIndex - 1;
+        }        
+    }
+    
+    return -1;
+    
+};
