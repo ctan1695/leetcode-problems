9a9bf357764ee037a8afadd5de94af8f9d295594
ctan1695 <71043594+ctan1695@users.noreply.github.com>
2021-08-14 17:21:59 -0500

    Time: 72 ms (80.27%), Space: 38.9 MB (85.26%) - LeetHub

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -3,37 +3,36 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    var stringCharacters = s.split('');
-    var parens = {
+    let parens = {
         '(': ')',
-        '[': ']',
-        '{': '}'
-    }
-
-    var parenKeys = Object.keys(parens);
+        '{': '}',
+        '[': ']'
+    };
     
-    if (!parens[stringCharacters[0]] || s.length % 2 !== 0) {
+    let openParens = '';
+    
+    if (s.length % 2 !== 0) {
         return false;
     }
-    var allOpenings = [];
-    allOpenings.push(stringCharacters[0]);
     
-    for (var i = 1; i < stringCharacters.length; i++) {
-        var currChar = stringCharacters[i];
-        var currOpening = allOpenings.slice(-1);
-
-        if (parenKeys.includes(currChar)) {
-            allOpenings.push(currChar);
-        } else if (parens[currOpening] !== currChar) {
-            return false;
+    for (let i = 0; i < s.length; i++) {        
+        let currentChar = s[i];
+        
+        if (parens[currentChar]) {
+            openParens += currentChar;
         } else {
-            allOpenings.pop();
+            let lastOpen = openParens[openParens.length - 1];
+            if (currentChar !== parens[lastOpen]) {
+                return false;
+            }
+            openParens = openParens.slice(0, openParens.length - 1);
         }
     }
-
-    if (allOpenings.length === 0) {
-        return true;
+    
+    if (openParens === '') {
+        return true;    
     } else {
         return false;
     }
+    
 };
