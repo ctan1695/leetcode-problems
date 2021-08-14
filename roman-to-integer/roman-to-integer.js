564b824908a8e03dfa4af3ef48da6f2af88ac66a
ctan1695 <71043594+ctan1695@users.noreply.github.com>
2021-08-14 17:00:29 -0500

    Time: 144 ms (72.27%), Space: 45 MB (50.47%) - LeetHub

--- roman-to-integer/roman-to-integer.js
+++ roman-to-integer/roman-to-integer.js
@@ -3,35 +3,29 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    var result = 0;
-    var glossary = {
-        'IV': 4,
-        'IX': 9,
-        'XL': 40,
-        'XC': 90,
-        'CD': 400,
-        'CM': 900,
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+    let glossary = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     
-    for (var i = 0; i < s.length; i++) {
-        if (i + 1) {
-          var pair = s[i] + s[i + 1];
-          var value = glossary[pair];
-          if (value) {
-            result += value;
-            i++;                
-          } else {
-          value = glossary[s[i]];
-          result += value;
-          } 
+    let result = 0;
+    let split = s.split('');
+    
+    for (let i = 0; i < split.length; i++) {
+        if ((split[i] === "I" && (split[i + 1] === "V" || split[i + 1] === "X")
+            || (split[i] === "X" && (split[i + 1] === "L" || split[i + 1] === "C"))
+            || (split[i] === "C" && (split[i + 1] === "D" || split[i + 1] == "M")))) {
+            result -= glossary[split[i]];
+        } else {
+            result += glossary[split[i]];
         }
+            
     }
+    
     return result;
 };
