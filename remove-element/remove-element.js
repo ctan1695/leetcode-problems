b3b9a0a3fd7f17a0782a091c9caff275b7d3e996
ctan1695 <71043594+ctan1695@users.noreply.github.com>
2021-09-03 17:56:31 -0500

    Time: 78 ms (33.30%), Space: 38.5 MB (92.15%) - LeetHub

--- remove-element/remove-element.js
+++ remove-element/remove-element.js
@@ -7,10 +7,8 @@ var removeElement = function(nums, val) {
     let pointerOne = 0;
     let pointerTwo = nums.length - 1;
     
-    while (pointerOne <= pointerTwo) {
-        let currentVal = nums[pointerOne];
-        
-        if (currentVal === val) {
+    while (pointerOne <= pointerTwo) {       
+        if (nums[pointerOne] === val) {
             nums[pointerOne] = nums[pointerTwo];
             nums[pointerTwo] = null;
             pointerTwo--;
