341d23e25305c2d2bffddadc847cf0c747dfd64d
ctan1695 <71043594+ctan1695@users.noreply.github.com>
2021-09-03 17:54:14 -0500

    Time: 96 ms (16.77%), Space: 38.6 MB (85.51%) - LeetHub

---
+++ remove-element/remove-element.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let pointerOne = 0;
+    let pointerTwo = nums.length - 1;
+    
+    while (pointerOne <= pointerTwo) {
+        let currentVal = nums[pointerOne];
+        
+        if (currentVal === val) {
+            nums[pointerOne] = nums[pointerTwo];
+            nums[pointerTwo] = null;
+            pointerTwo--;
+        } else {
+            pointerOne++;
+        }
+    }
+    
+    return pointerOne;
+};
