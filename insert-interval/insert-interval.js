a23692f68d1ec7ec5825897ff930adb6b4402ed3
ctan1695 <71043594+ctan1695@users.noreply.github.com>
2021-09-07 20:36:34 -0500

    Time: 96 ms (31.08%), Space: 40.5 MB (94.03%) - LeetHub

--- insert-interval/insert-interval.js
+++ insert-interval/insert-interval.js
@@ -4,11 +4,9 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    let insInterval = newInterval;
     let result = [];
     let inserted = false;
-    
-    
+
     for (let i = 0; i < intervals.length; i++) {
         if (inserted || newInterval[0] > intervals[i][1]) {
             result.push(intervals[i]);
@@ -18,13 +16,12 @@ var insert = function(intervals, newInterval) {
             inserted = true;
         } else {
             newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])];
-        }
+        }      
     }
 
     if (!inserted) {
         result.push(newInterval);
     }
-    
+       
     return result;
-    
 };
